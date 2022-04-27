4fd841b5a1f88058df36505b0eaf43ff77efc12c
fourat-bs <mohamedfouratb@gmail.com>
2022-04-27 05:37:00 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+required_packages = [
+    "numpy>=1.21.5",
+    "torch>=1.11.0",
+    "scikit-learn>=1.0.2",
+    "transformers>=4.18.0",
+    "joblib=1.1.0",
+]
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='text_normalizer',
+    version='0.0.1',
+    packages=find_packages(exclude=["tests", "examples"]),
+    url='https://github.com/fourat-bs/TextNormalizer',
+    license='MIT',
+    author='Fourat Ben Salah',
+    author_email='med.fourat.ben.salah@gmail.com',
+    description='TextNormalizer perform fully supervised text normalization',
+    long_description=long_description,
+    python_requires='>=3.7',
+    install_requires=required_packages,
+)
